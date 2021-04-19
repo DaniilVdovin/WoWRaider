657f70d40d49e964714cf89c0ea0e8ed727b7a91
Daniil Vdovin <stels1040533@gmail.com>
2021-04-19 16:40:32 +0300

    search

--- app/src/main/java/com/daniilvdovin/wowraider/MainActivity.java
+++ app/src/main/java/com/daniilvdovin/wowraider/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -12,9 +13,30 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Intent characterViewer = new Intent(this,CharacterViewer.class);
-        characterViewer.putExtra("request", new String[]{"eu", "Gordunni", "Эйлисанда"});
-        startActivity(characterViewer);
+
+        SearchView searchView = findViewById(R.id.search);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String[] qr;
+
+                if(query.contains("/"))
+                    qr =  query.split("/");
+
+                characterViewer.putExtra("request",qr);
+                startActivity(characterViewer);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+
+
     }
 }
