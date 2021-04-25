c895e79d6bd91552fa39bfb193c475035e3b6463
Daniil Vdovin <stels1040533@gmail.com>
2021-04-25 23:34:21 +0300

    edit warning

--- app/src/main/java/com/daniilvdovin/wowraider/jsonReader.java
+++ app/src/main/java/com/daniilvdovin/wowraider/jsonReader.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 class jsonReader {
 
@@ -28,7 +29,7 @@ class jsonReader {
         Log.e("readJsonFromUrl: ", url);
         InputStream is = new URL(url).openStream();
         try {
-            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             String jsonText = readAll(rd);
             JSONObject json = new JSONObject(jsonText);
             return json;
